3ed13071faf1901cc1a189f3dd294e67f3d5a4ae
ulisses953 <97630716+ulisses953@users.noreply.github.com>
2022-11-25 08:49:29 -0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace extratorDeInformacao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Ram memoria = new Ram();
+
+            for (var i = 0; i >= memoria.Velocidade.Count; i++)
+            {
+                label1.Text = memoria.Velocidade[i] + "   ";
+            }
+            
+
+
+        }
+    }
+}
---
+++ Ram.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace extratorDeInformacao
+{
+    public class Ram
+    {
+        private Process process = new Process();
+        public List<int> Velocidade {
+            get {
+                #region pegando informacao
+                Process velProcess = process;
+
+                velProcess.StartInfo.FileName = "wmic";
+                velProcess.StartInfo.Arguments = " memorychip get speed ";
+
+                velProcess.Start();
+                
+                var velNaoFormatado = velProcess.StandardOutput.ReadToEnd();
+                #endregion
+
+
+
+                velNaoFormatado = velNaoFormatado.Replace("Speed", "");
+                velNaoFormatado = limparStrig(velNaoFormatado);
+
+                var listaDeFrequencias = velNaoFormatado.Split(" ").Where(valor => valor != "");// aqi deu pau
+
+                //matando o processo
+                velProcess.Kill();
+
+                //return velNaoFormatado;
+                //return listaDeFrequencias.Select(frequencia => Convert.ToInt32(frequencia)).ToList(); 
+            }
+            private set { }
+        }
+
+        public Ram() { 
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardOutput = true;
+        }
+
+        private string limparStrig(string valor)
+        {
+            return valor.Trim()
+                .Replace("\r", "")
+                .Replace("\n", "");
+        }
+
+    }
+}
